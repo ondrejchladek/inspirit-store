af55f1f872eeec6fb595bac3420f008ecf4a1c49
ondrejchladek <ondrejchladek@gmail.com>
2024-02-29 12:53:24 +0100

    Changes to be committed: 	modified:   assets/icons/logo/index.js

--- assets/icons/logo/index.js
+++ assets/icons/logo/index.js
@@ -1,6 +1,6 @@
 const Logo = () => {
   return (
-    <img src="/logo.svg" alt="Logo" width="45.77" height="35.001" />
+    <img src="../../../public/images/logo.svg" alt="Logo" width="45.77" height="35.001" />
   );
 };
 
